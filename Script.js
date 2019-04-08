3980b64108f5f595fb9a11d5514f1ee61915e8f6
Murali Baskaran <49380115+muralibaski@users.noreply.github.com>
2019-04-08 11:28:50 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+ function myFunction()
+        {
+            var userName = document.getElementById("uname");
+            if(userName.value=="Murali")
+            {
+                userName.style="background:green";color:"white" ;
+            }
+            else
+            {
+                UserName.style="background:red";color:"yellow";
+            }
+                      
+        }
